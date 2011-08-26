ab70f9f3986d15267d0a42ed7b43b40956749fd2
Louis Taylor <kragniz@gmail.com>
2011-08-26 23:01:32 +0100

    created the main class structure of the reader

---
+++ bookReader.py
@@ -0,0 +1,17 @@
+#/usr/bin/env python
+
+class Book(object):
+    def __init__(self, textFileName):
+        pass
+    
+class BookHistory(object):
+    def __init__(self):
+        pass
+    
+class BookReader(object):
+    def __init__(self):
+        pass
+    
+if __name__ == '__main__':
+    #Main program bits
+    book = Book('example.txt')
