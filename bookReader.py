d20ca8153cb6574cca828d935e1b190800abb358
Louis Taylor <kragniz@gmail.com>
2011-08-27 02:09:35 +0100

    getting the next line now works.

--- bookReader.py
+++ bookReader.py
@@ -6,9 +6,9 @@ class Book(object):
     def __init__(self, textFileName):
         '''Create a book object'''
         self.lineLength = 80
-        text = open(textFileName, 'r')
+        self.text = open(textFileName, 'r')
         
-        self.continuedLine = ''
+        self.continuedWords = []
         
     def setLineLength(self, length):
         '''Set the length of each line of the output'''
@@ -19,11 +19,29 @@ class Book(object):
     
     def line(self):
         '''Return the next line'''
-        line = ''
+        line = []
+        words = []
+        
+        if self.continuedWords:
+            while self._checkLength(' '.join(line)) and self.continuedWords:
+                line += [self.continuedWords.pop(0)]
+                
+        if self._checkLength(' '.join(line)):
+            words = self._nextLine().split()
+            while self._checkLength(' '.join(line)) and words:
+                line += [words.pop(0)]
+        else:
+            self.continuedWords = [line.pop(-1)] + self.continuedWords
+            
+        if not self._checkLength(' '.join(line)):
+            self.continuedWords = [line.pop(-1)] + self.continuedWords
+            
+        self.continuedWords += words
+        return ' '.join(line)
             
     def _nextLine(self):
         '''Return a single line from the book'''
-        return text.read()
+        return self.text.readline()
     
 class BookHistory(object):
     '''Make and write the settings/progress for a particular book'''
@@ -38,3 +56,8 @@ class BookReader(object):
 if __name__ == '__main__':
     #Main program bits
     book = Book('example.txt')
+    print book.line()
+    print book.line()
+    print book.line()
+    print book.line()
+    print book.line()
